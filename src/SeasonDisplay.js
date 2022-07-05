90fa66d3eb2844095774d7919db2701689ee06d7
afraze <melissututar@gmail.com>
2022-07-05 22:52:03 +0200

    The changes after opening in visual studio

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,28 +1,37 @@
-.icon-left {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-}
-.icon-right {
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-}
-.season-display.winter i {
-  color: blue;
-}
-.season-display.summer i {
-  color: red;
-}
-.season-display {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-.summer {
-  background-color: orange;
-}
-.winter {
-  background-color: aliceblue;
-}
+import "./SeasonDisplay.css";
+import React from "react";
+
+const SeasonDisplay = (props) => {
+  console.log(props.latitude);
+  const season = getSeason(props.latitude, new Date().getMonth());
+  console.log("season: " + season);
+  const { text, iconName } = seasonConfig[season]; //{text,iconName}
+  console.log({ text, iconName });
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
+};
+
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Brrr it is cold",
+    iconName: "snowflake",
+  },
+};
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? "winter" : "summer";
+  } else {
+    return latitude > 0 ? "winter" : "summer";
+  }
+};
+
+export default SeasonDisplay;
